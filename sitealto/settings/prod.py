28ce4369824a3eff8af7f51377fff0067535e728
nycolasgh <nycolashartkopf@gmail.com>
2022-12-15 22:44:58 -0300

    Railway Database Config

--- sitealto/settings/prod.py
+++ sitealto/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
-
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -9,9 +9,8 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
