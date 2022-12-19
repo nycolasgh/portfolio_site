2089c2eabe7c4e63acef814fcb4b8b8e19d45953
nycolasgh <nycolashartkopf@gmail.com>
2022-12-19 15:31:24 -0300

    SSL and CSRF configs

--- sitealto/settings/prod.py
+++ sitealto/settings/prod.py
@@ -4,7 +4,7 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://web-production-1c31.up.railway.app']
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -14,3 +14,13 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+
+CORS_REPLACE_HTTPS_REFERER = True
+HOST_SCHEME = "https://"
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 1000000
+SECURE_FRAME_DENY = True
