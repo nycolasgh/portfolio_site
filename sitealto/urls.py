0fb83027c8575504fc1b91e48724159ae0bcca34
“nycolasgh” <nycolashartkopf@gmail.com>
2023-08-08 19:56:44 -0300

    teste

--- sitealto/urls.py
+++ sitealto/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls'))
-] #  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] #  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) > Para usar localmente)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
